786b38ba72f7ae4cc0ae800249c3ac7449354a11
Vince Kegel <vkegel@gmail.com>
2017-02-05 15:29:55 -0500

    Updated code to solve programming assignment
    Used the Solve function, instead of the Mean as shown in the example.

--- cachematrix.R
+++ cachematrix.R
@@ -2,8 +2,20 @@
 ## functions do
 
 ## Write a short comment describing this function
+## follow the model of the 'mean' example, but use the solve function for Inverse matrices
 
 makeCacheMatrix <- function(x = matrix()) {
+    invrs<-NULL
+    set <- function(y) {
+      x<<-y
+      invrs<<-NULL
+    }
+    get <- function () x
+    setinv <-function(inverse) invrs<<-inverse
+    getinv <- function() invrs
+    list (set = set, get = get,
+          setinv = setinv,
+          getinv = getinv)
 
 }
 
@@ -12,4 +24,15 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+              inv <- x$getinv()
+      if (!is.null(inv)) {
+        message("getting cached data..")
+        return(inv)
+      }
+      mat <-x$get()
+      inv <- solve(mat,...)
+      x$setinv(inv)
+      inv
+
+        
 }
